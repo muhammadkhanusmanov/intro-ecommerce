6ce3bf1da5b92bb963d5a7b0f056333feb07911c
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-04-02 09:59:41 +0500

    create ProductView

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# import views
+from .views import (
+    ProductView,
+)
+
+urlpatterns = [
+   path('products', ProductView.as_view()), 
+   path('products/<int:id>', ProductView.as_view()), 
+   path('products/', ProductView.as_view()),
+]
