5eb1cf40b5b57c3e8086b202316d0851d55cd590
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-04-03 10:29:30 +0500

    create get_products_gategory function url

--- api/urls.py
+++ api/urls.py
@@ -5,16 +5,20 @@ from .views import (
     update_product,
     delete_product,
     CompanyView,
-    get_company_products
+    get_company_products,
+    CategoryView,
+    get_category_products
 )
 
 urlpatterns = [
-   path('products', ProductView.as_view()), 
+   path('products/', ProductView.as_view()), 
    path('products/<int:id>', ProductView.as_view()), 
-   path('products/', ProductView.as_view()),
    path('products/<int:pk>/update/',update_product),
    path('products/<int:pk>/delete/',delete_product),
    path('companies/', CompanyView.as_view()),
    path('companies/<int:id>',CompanyView.as_view()),
-   path('companies/<int:id>/products/',get_company_products)
+   path('companies/<int:id>/products/',get_company_products),
+   path('categories/', CategoryView.as_view()),
+   path('categories/<int:id>',CategoryView.as_view()),
+   path('categories/<int:pk>/products/',get_category_products),
 ]
